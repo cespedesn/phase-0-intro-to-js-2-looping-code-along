a522633c6c0b6cf38c3c548c59c4535359d13424
Neftali Cespedes <cesinvestmentholdings@gmail.com>
2022-08-30 14:18:47 -0400

    commit tree a15781e740f90ed15889c29b34078f2b812beb39

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solutions in this file
+/*const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+*/
+
+function writeCards(names, event) {
+    const thankYouMessage = [];
+    for (let i = 0; i < names.length; i++) {
+        console.log(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+        thankYouMessage.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    console.log(thankYouMessage);
+    return thankYouMessage;
+    
+}
+function countDown(number){
+    let loop = number;
+    while (loop >= 0) {
+    console.log(loop --);    
+    }
+}
